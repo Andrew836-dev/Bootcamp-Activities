2a6e06b98cd55a907555a26d29e14a375b32a67e
Andrew Gray <andrew.aj.gray@gmail.com>
2020-06-06 12:12:55 +0930

    todo list that can remove items from the list, and use local storage

--- 04-week/todos/script.js
+++ 04-week/todos/script.js
@@ -5,28 +5,53 @@ var todoCountSpan = document.querySelector("#todo-count");
 
 var todos = ["Learn HTML", "Learn CSS", "Learn JavaScript"];
 
-renderTodos();
+init();
 
-document.addEventListener('keydown', function(event) {
-    if (event.keyCode == 13) {
-        event.preventDefault();
-        if (todoInput.value == "") {
-            return;
-        }
-        else {
-            todos.push(todoInput.value);
-            todoInput.value = "";
-            renderTodos();
-        }
+document.addEventListener('submit', function (event) {
+    event.preventDefault();
+    if (todoInput.value.trim() == "") {
+        return;
+    }
+    else {
+        todos.push(todoInput.value.trim());
+        todoInput.value = "";
+        renderTodos();
     }
 });
+
+document.addEventListener('click', function (event) {
+    if (event.target.matches("button")) {
+        // console.log(event.target.parentElement);
+        var index = parseInt(event.target.parentElement.getAttribute("data-index"));
+        todos.splice(index, 1);
+        renderTodos();
+    }
+})
+
+function init() {
+    var storedTodos = JSON.parse(localStorage.getItem("todos"));
+    if (storedTodos) {
+        todos = storedTodos;
+    }
+    renderTodos();
+}
+
+function storeTodos() {
+    console.log("storing");
+    localStorage.setItem("todos", JSON.stringify(todos));
+}
 
 function renderTodos() {
     todoList.innerHTML = "";
     todoCountSpan.textContent = todos.length;
     for (var i = 0; i < todos.length; i++) {
         var li = document.createElement('li');
+        var btn = document.createElement('button');
         li.textContent = todos[i];
+        btn.textContent = "Complete";
+        li.setAttribute("data-index", i)
         todoList.append(li);
+        li.append(btn);
     }
+    storeTodos();
 }
