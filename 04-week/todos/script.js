f97e9ef2e2fc9719db6a1cb6f5ce099a5571f6bf
Andrew Gray <andrew.aj.gray@gmail.com>
2020-06-06 12:30:48 +0930

    added comments to clarify todo list

--- 04-week/todos/script.js
+++ 04-week/todos/script.js
@@ -5,53 +5,85 @@ var todoCountSpan = document.querySelector("#todo-count");
 
 var todos = ["Learn HTML", "Learn CSS", "Learn JavaScript"];
 
+// initalise the list on page load
 init();
 
+// listen for a submit event
 document.addEventListener('submit', function (event) {
+    // prevent the page from refreshing
     event.preventDefault();
+    // check if the input has any useful data
     if (todoInput.value.trim() == "") {
+        // if there's no text, end the function
         return;
     }
     else {
+        // add new entry to array
         todos.push(todoInput.value.trim());
+        // clear the input field
         todoInput.value = "";
+        // update the display
         renderTodos();
     }
 });
 
+// listen for clicks
 document.addEventListener('click', function (event) {
+    // if the click target is a button, continue
     if (event.target.matches("button")) {
-        // console.log(event.target.parentElement);
+        // turn the data-index attribute into a number
         var index = parseInt(event.target.parentElement.getAttribute("data-index"));
+        // remove the entry at that index
         todos.splice(index, 1);
+        // update the display
         renderTodos();
     }
 })
 
+// retrieves from local storage
 function init() {
+    // JSON used to retrieve an array from local storage
     var storedTodos = JSON.parse(localStorage.getItem("todos"));
+    // if storedTodos exists...
     if (storedTodos) {
+        // update the main array with the stored data
         todos = storedTodos;
     }
+    // update the display
     renderTodos();
 }
 
+// function to store the array into local storage
 function storeTodos() {
-    console.log("storing");
+    // using JSON to turn the array into a single string
     localStorage.setItem("todos", JSON.stringify(todos));
 }
 
+// function to update the display
 function renderTodos() {
+    // removes everything inside the list
     todoList.innerHTML = "";
+    // updates the counter to match the number of items in the array
     todoCountSpan.textContent = todos.length;
+    // loop to add everything in the array to the list
     for (var i = 0; i < todos.length; i++) {
         var li = document.createElement('li');
         var btn = document.createElement('button');
+
+        // setting parameters of the li object
         li.textContent = todos[i];
-        btn.textContent = "Complete";
         li.setAttribute("data-index", i)
+        // add the li object to the view
         todoList.append(li);
+
+        // setting parameters of the button object
+        btn.textContent = "Complete";
+
+        // add the button to the current list item
         li.append(btn);
+        
+        // return to top of loop
     }
+    // save list to local storage
     storeTodos();
 }
