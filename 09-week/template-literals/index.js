d762d1398ab0b7f825bbeea229280da2f02073eb
Andrew Gray <andrew.aj.gray@gmail.com>
2020-07-09 20:40:35 +0930

    Introduction to template literals

---
+++ 09-week/template-literals/index.js
@@ -0,0 +1,21 @@
+// music should be an object with title, artist, and album properties
+const music = {
+  // code here
+  title: "Crawling In My Skin",
+  artist: "Linkin Park",
+  album: "Hybrid Theory"
+};
+
+// Write code between the <div> tags to output the data from the music object above.
+// Use an h2 element for the title and a p element for artist and title
+const songSnippet = `
+  <div class="song">
+  <h2>${music.title}</h2>
+  <p>${music.artist}</p>
+  <p>${music.album}</p>
+  </div>
+`;
+
+const element = document.getElementById("music");
+element.innerHTML = songSnippet;
+
